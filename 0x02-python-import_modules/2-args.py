888530faffb25344c33268c0962241bb5bd5fc93
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2021-04-29 22:31:19 +0000

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,6 +3,8 @@ if __name__ == "__main__":
  import sys
  if (len(sys.argv)) == 1:
   print("{} arguments.".format((len(sys.argv)) - 1))
+ elif (len(sys.argv)) == 2:
+  print("{} argument:".format((len(sys.argv)) - 1)) 
  else:
   print("{} arguments:".format((len(sys.argv)) - 1))
  for i in range(1, len(sys.argv)):
