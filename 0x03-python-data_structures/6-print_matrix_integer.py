fc289f661203ba4684df6c13005aa67463c0a4c9
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2021-05-04 21:40:46 +0000

    6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,5 @@
 def print_matrix_integer(matrix=[[]]):
 for rows in matrix: 
     for columns in rows: 
-        print("{:d}".format(matrix[rows][columns]), end=' ')        
+        print("{:d}".format(matrix[rows][columns]), end=' ' if not (columns == len(matrix[rows]) - 1):)        
         print() 
