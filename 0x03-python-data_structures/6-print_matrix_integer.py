dcab35181300b8b95350ce09756a00526ba70d76
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2021-05-04 21:23:09 +0000

    6-print_matrix_integer.py

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+for i in range(R): 
+    for j in range(C): 
+        print("{:d}".format(matrix[i][j]), end=' ')        
+        print() 
