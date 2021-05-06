32b873e46c6d1a2e634232a8721d3461a94dde05
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2021-05-06 16:50:41 +0000

    12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+ if (roman_string is None) or type(roman_string) != str:
+  return 0
  rom_val = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
  int_val = 0
  for i in range(len(roman_string)):
