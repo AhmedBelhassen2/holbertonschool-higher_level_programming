7aecc67b43a985707a8d12fcecc12ec28133c3f4
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2021-05-23 12:49:32 +0000

    0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -11,8 +11,7 @@ b is the second integer
 def add_integer(a, b=98):
     """add_integer(a, b) function that adds two integers/floats
     a is the first integer
-    b is the second integer
-    """
+    b is the second integer"""
 
     if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
