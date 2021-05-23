fd1569bafcd9235ce73cc8b925c027fbb4614909
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2021-05-23 19:38:24 +0000

    all

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -9,22 +9,27 @@ def matrix_divided(matrix, div):
     """
     function that divides all elements of a matrix.
     """
-    if type(matrix) is not list:
-        raise TypeError("matrix must be a matrix (list of lists) of integers/float")
-    for i in range(len(matrix)):
-        if type(matrix[i]) is not list:
+    if type(matrix) is not list or matrix == [[]]:
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    for i in matrix:
+        if type(i) is not list:
             raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-        if i != len(matrix) -1:
-            if len(matrix[i]) != len(matrix[i + 1]):
-                raise TypeError("Each row of the matrix must have the same size")
-        for elem in matrix[i]:
-            if type(elem) is not int and type(elem) is not float:
-                raise TypeError(""matrix must be a matrix (list of lists) of integers/floats"")
+    for i in matrix:
+        for j in i:
+            if type(j) is not int and type(j) is not float:
+                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        size = len(matrix[0])
+    for i in matrix:
+        if len(i) is not size:
+            raise TypeError("Each row of the matrix must have the same size")
     if type(div) is not int and type(div) is not float:
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            new_matrix[i][j] = round(matrix[i][j] / div, 2)
+    new_matrix = []
+    for i in matrix:
+        row = []
+        for j in i:
+            row.append(round((j / div), 2))
+        new_matrix.append(row)
     return new_matrix
