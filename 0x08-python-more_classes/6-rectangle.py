413d42ec6132b96ef980a44347d1f180d97d31dc
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2021-05-24 22:36:06 +0000

    6-rectangle.py

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -6,6 +6,8 @@ module contains one class
 
 class Rectangle:
     """ class Rectangle """
+    number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         """ instantiation """
         self.height = height
