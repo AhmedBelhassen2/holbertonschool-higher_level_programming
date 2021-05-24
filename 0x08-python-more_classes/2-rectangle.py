d1e72295f078c8f3d713c95759e28ae39ab534a2
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2021-05-24 22:13:05 +0000

    2-rectangle.py

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -38,6 +38,7 @@ class Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
+
     def area(self):
         """ returns area of the rectangle """
         return self.__height * self.__width
