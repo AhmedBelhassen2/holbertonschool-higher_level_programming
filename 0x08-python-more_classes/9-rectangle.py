b6363be80452ac6b9649f708bd17ddf99bd14f83
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2021-05-24 23:00:26 +0000

    9-rectangle.py

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -15,6 +15,17 @@ class Rectangle:
         """returns a new Rectangle instance that is a square w/ h==w==size"""
         return cls(size, size)
 
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        """returns the biggest rectangle based on the area"""
+        if type(rect_1) is not Rectangle:
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if type(rect_2) is not Rectangle:
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        if rect_1.area() >= rect_2.area():
+            return rect_1
+        return rect_2
+
         def __init__(self, width=0, height=0):
         """ instantiation """
         self.height = height
@@ -78,15 +89,3 @@ class Rectangle:
         """ called when an object is deleted with del """
         Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
-
-    @staticmethod
-    def bigger_or_equal(rect_1, rect_2):
-        """ returns the biggest rectangle based on the area """
-        if type(rect_1) is not Rectangle:
-            raise TypeError("rect_1 must be an instance of Rectangle")
-        if type(rect_2) is not Rectangle:
-            raise TypeError("rect_2 must be an instance of Rectangle")
-        if rect_1.area() >= rect_2.area():
-            return rect_1
-        else:
-            return rect_2
