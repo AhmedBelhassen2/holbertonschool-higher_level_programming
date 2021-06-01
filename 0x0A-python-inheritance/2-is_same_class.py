fecac4a7b97cbf2f5270c01af7ca115a8d1c3ee9
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2021-06-01 22:43:02 +0000

    2-is_same_class.py

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+is same class fn
+"""
+
+
+def is_same_class(obj, a_class):
+    """ same class  """
+    if type(obj) == a_class:
+        return True
+    else:
+        return False
