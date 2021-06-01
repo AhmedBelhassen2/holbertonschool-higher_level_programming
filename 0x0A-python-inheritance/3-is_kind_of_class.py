14117afea0cec57e9da3d805fec9115fb195e2fd
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2021-06-01 22:46:55 +0000

    3-is_kind_of_class.py

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+is kin of class fn
+"""
+
+
+def is_same_class(obj, a_class):
+    """ kin of class  """
+    if isinstance(obj) == a_class:
+        return True
+    else:
+        return False
