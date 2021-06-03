72306bde28e4b9db928613abd29dbe1a585db76a
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2021-06-03 13:24:51 +0000

    6-load_from_json_file.py

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+JSON
+"""
+import json
+
+
+def load_from_json_file(filename):
+    """ reates an Object from a “JSON file” """
+    with open(filename, encoding="utf-8") as f:
+        return json.load(f)
