6c5bf091f7e26a3f5135c1d1041fe0aedd4d8804
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2021-06-03 22:45:48 +0000

    7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -12,8 +12,8 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 args = sys.argv[1:]
 with open('add_item.json', 'a') as f:
     try:
-        list = load_from_json_file('add_item.json')
+        l = load_from_json_file('add_item.json')
     except:
-        list = []
-    list += args
-    save_to_json_file(list, 'add_item.json')
+        l = []
+    l += args
+    save_to_json_file(l, 'add_item.json')
