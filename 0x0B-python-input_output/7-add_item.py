edbfabc1f04f301898fe110f1974bbe99b15fa70
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2021-06-03 22:43:40 +0000

    7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,15 +4,16 @@ JSON add
 """
 
 
+import json
 import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file')
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 args = sys.argv[1:]
-with open("add_item.json", "a")
+with open('add_item.json', 'a') as f:
     try:
-        list = load_from_json_file("add_item.json")
-    except Exception:
+        list = load_from_json_file('add_item.json')
+    except:
         list = []
     list += args
-    save_to_json_file(list, "add_item.json")
+    save_to_json_file(list, 'add_item.json')
