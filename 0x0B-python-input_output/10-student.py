829cf67e90c42815c858523ec9b62458cfd92766
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2021-06-04 10:44:26 +0000

    10-student.py

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" simple module """
+
+
+class Student:
+    '''Student class
+    '''
+
+    def __init__(self, first_name, last_name, age):
+        '''defines a student
+        '''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return (self.__dict__)
+        else:
+            d = {}
+            for names in attrs:
+                if asattr(self, names):
+                    d[names] = getattr(self, names)
+            return (d)
