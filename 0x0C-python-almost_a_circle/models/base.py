3184e106a67e098e54cc0b8d66b443f4b23876a2
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2021-06-09 14:20:49 +0000

    all

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -35,3 +35,10 @@ class Base:
             l.append(cls.to_dictionary(j))
         with open('{}.json'.format(cls.__name__), "w") as file:
             file.write(cls.to_json_string(l))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """ static method that deserializes json_string """
+        if json_string is None:
+            return "[]"
+        return json.loads(json_string)
