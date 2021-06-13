0764f25bad3f8fe83ff8e3c09426bb2ce1eae8fe
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2021-06-13 18:37:50 +0000

    all

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -16,33 +16,27 @@ def matrix_divided(matrix, div):
             ZeroDivisionError: Div is 0
         Return : New matrix Devided
         """
+    count = 0
+    Error1 = 'matrix must be a matrix (list of lists) of integers/floats'
+    Error2 = 'Each row of the matrix must have the same size'
+
     if isinstance(matrix, list) is False:
-        raise TypeError(
-                "matrix must be a matrix "
-                "(list of lists) of integers/floats")
+        raise TypeError(Error1)
 
     if len(matrix) is 0 or matrix == [[]]:
-        raise TypeError(
-                "matrix must be a matrix "
-                "(list of lists) of integers/floats")
+        raise TypeError(Error1)
     for elm in matrix:
         if isinstance(elm, list) is False:
-            raise TypeError(
-                "matrix must be a matrix "
-                "(list of lists) of integers/floats")
+            raise TypeError(Error1)
         for n in elm:
             if type(n) is not int and type(n) is not float:
-                raise TypeError(
-                        "matrix must be a matrix "
-                        "(list of lists) of integers/floats")
+                raise TypeError(Error1)
 
     if len(matrix) > 1:
         count = len(matrix[1])
         for elm in matrix:
             if count is not len(elm):
-                raise TypeError(
-                        "Each row of the matrix"
-                        "must have the same size")
+                raise TypeError(Error2)
     if type(div) is not int and type(div) is not float:
         raise TypeError('div must be a number')
 
