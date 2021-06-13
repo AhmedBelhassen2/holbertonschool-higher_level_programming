719a9c6ee900c86171b34e33cb1eeaa907c739f4
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2021-06-13 21:06:44 +0000

    all

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -13,12 +13,12 @@ def text_indentation(text):
     if type(text) is not str:
         raise TypeError("text must be a string")
 
-    for char in text:
-        st += char
-
-        if char in special:
-            print(st.strip())
+    a = 0
+    while a < len(text):
+        if text[a] in [':', '.', '?']:
+            print(text[a])
             print()
-            st = ''
-
-    print(st.strip(), end='')
+            a += 1
+        else:
+            print(text[a], end='')
+        a += 1
