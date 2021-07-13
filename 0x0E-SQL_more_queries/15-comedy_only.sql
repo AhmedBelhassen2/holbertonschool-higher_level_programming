05a5fc912a3a62e39ea0caed5357b35396e3b8c5
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2021-07-13 22:42:34 +0000

    15-comedy_only.sql

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,7 @@
+-- lists all Comedy shows in the database hbtn_0d_tvshows.
+SELECT title
+FROM tv_genres
+LEFT JOIN tv_show_genres ON genre_id = tv_genres.id
+LEFT JOIN tv_shows ON show_id = tv_shows.id
+WHERE name = "Comedy"
+ORDER BY title ASC;
