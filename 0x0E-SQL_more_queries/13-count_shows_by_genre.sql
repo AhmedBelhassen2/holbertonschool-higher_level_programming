340736fc3616d84cdf43daeb79b7ebd5bce685ec
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2021-07-13 22:38:04 +0000

    13-count_shows_by_genre.sql

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,6 @@
+-- lists all genres from hbtn_0d_tvshows
+SELECT tv_genres.name AS genre, count(*) AS number_of_shows
+FROM tv_show_genres
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+GROUP BY tv_genres.name
+ORDER BY number_of_shows DESC;
