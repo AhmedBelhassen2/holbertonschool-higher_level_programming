d60674b456982676130bb565da0c2dc846fc9f6b
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2021-08-04 20:48:33 +0000

    6-square.js

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const Rectangle = require('./5-square');
+module.exports = class Square extends Rectangle {
+  charPrint (c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) console.log(c.repeat(this.width));
+    }
+  }
+};
