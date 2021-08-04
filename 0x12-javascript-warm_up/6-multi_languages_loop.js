8b5c8aa5c8962ad9f8314b94b60359502ec66fc6
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2021-08-04 23:29:10 +0000

    6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const list = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const list = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let s = 0; s < list.length; s++) {
   console.log(list[s]);
 }
