3085d13ba5acd7445c9c595fcc7e97b5fc5e64e4
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2021-08-06 23:31:17 +0000

    2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-''' takes in an argument and displays all values in the states table '''
+''' takes in an argument and displays '''
 if __name__ == "__main__":
     import MySQLdb
     import sys
