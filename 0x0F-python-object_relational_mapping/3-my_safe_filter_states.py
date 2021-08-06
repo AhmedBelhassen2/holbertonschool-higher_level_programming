c23e3c72313edc55d85e4b5d30e038e3b6d491e9
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2021-08-06 23:42:00 +0000

    3-my_safe_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+''' takes in an argument and displays '''
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+
+    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3])
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE BINARY name=%s ORDER BY id ASC",
+                (sys.argv[4],))
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
