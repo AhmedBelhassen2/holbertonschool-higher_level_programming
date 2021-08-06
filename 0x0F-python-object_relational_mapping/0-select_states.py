b69c87eb9c7570748c8c74a2b16c84a1baaf684a
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2021-08-06 23:10:00 +0000

    all

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+''' Module to print all states '''
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+
+    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3])
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
