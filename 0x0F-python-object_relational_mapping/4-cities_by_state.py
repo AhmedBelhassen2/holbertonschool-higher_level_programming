fcc08b03e6366b11c54a4580e6db868e8f1007bb
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2021-08-06 23:48:43 +0000

    4-cities_by_state.py

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+''' takes in an argument and displays '''
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+
+    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3])
+    cur = conn.cursor()
+    cur.execute("SELECT c.id, c.name, s.name "
+                "FROM cities AS c JOIN states AS s ON c.state_id = s.id "
+                "ORDER BY c.id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
