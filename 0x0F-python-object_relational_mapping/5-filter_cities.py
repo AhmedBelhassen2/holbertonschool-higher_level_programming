8cf085fecb6330dca60b5df128ceb9667e18a04b
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2021-08-07 00:24:06 +0000

    5-filter_cities.py

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     cur.execute("SELECT cities.name FROM cities \
     JOIN states ON cities.state_id = states.id WHERE states.name LIKE %s \
     ORDER BY cities.id", (sys.argv[4],))
-    query_rows = cur.fetchall()
-    print(", ".join(row[0] for row in query))
+    query = cur.fetchall()
+    print(", ".join([row[0] for row in query]))
     cur.close()
     conn.close()
