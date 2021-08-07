6661d5a5509fe696783b50984e9b3f9baec0dc39
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2021-08-07 01:13:23 +0000

    all

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -10,6 +10,8 @@ from model_state import Base, State
 class city(Base):
     """definition of city class"""
     __tablename__ = 'cities'
-    id = Column(Integer, primary_key=True)
+
+    id = Column(Integer, primary_key=True, autoincrement=True, unique=True,
+                nullable=False)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'))
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
