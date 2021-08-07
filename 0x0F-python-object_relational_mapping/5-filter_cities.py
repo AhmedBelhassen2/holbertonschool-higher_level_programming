0ffe9e168c572fb07e3bbaa289bf199e33efe58f
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2021-08-07 00:20:16 +0000

    5-filter_cities.py

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -7,9 +7,9 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                            passwd=sys.argv[2], db=sys.argv[3])
     cur = conn.cursor()
-    cur.execute("SELECT c.name FROM cities AS c INNER JOIN states AS s ON "
-                "c.state_id = s.id WHERE s.name = %s "
-                "ORDER BY c.id ASC", (sys.argv[4],))
+    cur.execute("SELECT cities.name FROM cities \
+    JOIN states ON cities.state_id = states.id WHERE states.name LIKE %s \
+    ORDER BY cities.id", (sys.argv[4],))
     query_rows = cur.fetchall()
     print(", ".join(row[0] for row in query))
     cur.close()
