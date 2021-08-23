7c1aa88f56288a6adfd1e02514fa6cbd7f60c027
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2021-08-23 01:58:26 +0000

    7-error_code.py

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
-"""Send request"""
+"""Write a Python script that takes in a URL, sends a request to the URL"""
 import sys
 import requests
 
 
 if __name__ == "__main__":
-    req = sys.argv[1]
+    req = requests.get(sys.argv[1])
     if req.status_code >= 400:
         print("Error code: {}".format(req.status_code))
     else:
