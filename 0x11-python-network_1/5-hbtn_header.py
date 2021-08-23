e13fe67b4678843732891e7d215bf45e46cdc845
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2021-08-23 01:48:11 +0000

    5-hbtn_header.py

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
-"""Take URL"""
+"""Write a Python script that takes in a URL, sends a request to the URL"""
 
 import requests
 import sys
 
 if __name__ == "__main__":
-     URL = requests.get(sys.argv[1])
-     print(URL.headers.get('x-request-id'))
+    URL = requests.get(sys.argv[1])
+    print(URL.headers.get('x-request-id'))
